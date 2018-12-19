6639fe41a9f29c55ad5da135fa17978160ee0aff
EC2 Default User <ec2-user@ip-172-31-35-84.us-east-2.compute.internal>
2018-12-19 10:53:31 +0000

    change sql home and sneakers#index

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,6 +1,6 @@
 class SneakersController < ApplicationController
   def index
-    @sneakers = Sneaker.select(:id,:sneakers_ref, :brand, :title, :img_url, :color).group(:sneakers_ref).having("count(*) > 1")
+    @sneakers = Sneaker.select(:id,:sneakers_ref, :brand, :title, :img_url, :color).group(:sneakers_ref).having("count(*) > 1").order(:id)
     end
   
   def create
