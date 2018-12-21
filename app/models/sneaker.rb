1867e99868cb7208ee0add3d1360c3420a0f032a
EC2 Default User <ec2-user@ip-172-31-35-84.us-east-2.compute.internal>
2018-12-21 13:06:19 +0000

    add scopes to sneaker.rb

--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -16,33 +16,25 @@ class Sneaker < ApplicationRecord
  validates :color, presence: true
  validates :img_url, presence: true
  
- scope :search_sneakers_ref, lambda { |sneakers_ref| 
- where(sneakers_ref: param)
  
- return nil  if sneakers_ref.blank?
-
-  # condition query, parse into individual keywords
-  terms = sneakers_ref.downcase.split(/\s+/)
-
-  # replace "*" with "%" for wildcard searches,
-  # append '%', remove duplicate '%'s
-  terms = terms.map { |e|
-    (e.gsub('%', '%') + '%').gsub(/%+/, '%')
-  }
-  # configure number of OR conditions for provision
-  # of interpolation arguments. Adjust this if you
-  # change the number of OR conditions.
-  num_or_conds = 1
-  where(
-    terms.map { |term|
-      "(LOWER(students.first_name) LIKE ? OR LOWER(students.last_name) LIKE ?)"
-    }.join(' AND '),
-    *terms.map { |e| [e] * num_or_conds }.flatten
-  )
-  }
-  
-  def self.options_for_select
-  order('LOWER(sneakers_ref)').map { |e| [e.sneakers_ref, e.id] }
-end
+ scope :search_sneakers_ref, lambda {
+  where(sneakers_ref: params[:search_sneakers_ref])
+ }
+ 
+ scope :search_brand, lambda {
+  where(brand: params[:search_brand])
+ }
+ 
+ scope :search_title, lambda {
+  where(title: params[:search_title])
+ }
+ 
+ scope :search_color, lambda {
+  where(color: params[:search_color])
+ }
+ 
+ def self.options_for_select
+ order('LOWER(sneakers_ref)').map { |e| [e.sneakers_ref, e.id] }
+ end
 
 end
