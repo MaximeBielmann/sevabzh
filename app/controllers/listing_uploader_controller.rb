6f2bb10b8af1323b6a05879d6c4a6669f0a4695e
EC2 Default User <ec2-user@ip-172-31-35-84.us-east-2.compute.internal>
2018-12-31 17:18:18 +0000

    try to change the listing uploader

--- app/controllers/listing_uploader_controller.rb
+++ app/controllers/listing_uploader_controller.rb
@@ -1,12 +1,14 @@
 class ListingUploaderController < ApplicationController
-    require "csv"
+    require 'csv'
     
     def upload
         @import = ListingUploader.new
         @import.store!(params[:file])
         
-        # CSV.foreach(@import.current_path, headers: true) do |row|
-        #     Sneaker.create!(row.to_h)
-        # end
+        CSV.foreach(@import.current_path, headers: true) do |row|
+            Sneaker.create sneakers_ref: row['sneakers_ref'], brand: row['brand'], title: row['title'], color: row['color'], img_url: row['img_url']
+            redirect_to "/sneakers/"
+        end
+        redirect_to "/sneakers/"
     end
 end
