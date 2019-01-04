d147aaa9b1bf592c6d16748bb6cea8c4f74b81a7
EC2 Default User <ec2-user@ip-172-31-35-84.us-east-2.compute.internal>
2019-01-04 12:36:16 +0000

    try to just store

--- app/controllers/listing_uploader_controller.rb
+++ app/controllers/listing_uploader_controller.rb
@@ -4,14 +4,14 @@ class ListingUploaderController < ApplicationController
     def upload
         @import = ListingUploader.new
         @import.store!(params[:file])
-        @import.retrieve_from_store!(params[:file])
+        # @import.retrieve_from_store!(params[:file])
         
-        CSV.foreach(@import.current_path, headers: true) do |row|
-            Sneaker.create sneakers_ref: row['sneakers_ref'], brand: row['brand'], title: row['title'], color: row['color'], img_url: row['img_url']
-            # Stock.create rajouter les colonnes du stock
-            # faire un if pour le cas où la sneaker ou le stock existent déjà dans la table
-            redirect_to "/sneakers/"
-        end
+        # CSV.foreach(@import.current_path, headers: true) do |row|
+        #    Sneaker.create sneakers_ref: row['sneakers_ref'], brand: row['brand'], title: row['title'], color: row['color'], img_url: row['img_url']
+        #    # Stock.create rajouter les colonnes du stock
+        #    # faire un if pour le cas où la sneaker ou le stock existent déjà dans la table
+        #    redirect_to "/sneakers/"
+        # end
         redirect_to "/sneakers/"
     end
 end
