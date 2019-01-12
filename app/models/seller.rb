9898fcd1acb39d75f852a7fc08de6202b8c8483f
EC2 Default User <ec2-user@ip-172-31-35-84.us-east-2.compute.internal>
2019-01-12 17:40:38 +0000

    change margin to paddin in sellers#index

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,6 +1,6 @@
 class Seller < ApplicationRecord
     has_many :stocks
-    has_many :stocks
+    has_many :sneakers
     
     validates :vendor, presence: true, uniqueness: true
     validates :logo_url, presence: true
