6e06a298c2a37369c375287a32e35e362910b5e4
EC2 Default User <ec2-user@ip-172-31-35-84.us-east-2.compute.internal>
2019-01-13 16:48:49 +0000

    error

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -2,6 +2,8 @@ class Seller < ApplicationRecord
     has_many :stocks
     has_many :sneakers
     
+    attr_accessor :vendor
+    
     validates :vendor, presence: true, uniqueness: true
     validates :logo_url, presence: true
 end
