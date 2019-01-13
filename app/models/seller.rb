29dcd890560c9a6798074e1f88026ca7b60ece75
EC2 Default User <ec2-user@ip-172-31-35-84.us-east-2.compute.internal>
2019-01-13 16:51:14 +0000

    remove attr accessor

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -2,8 +2,6 @@ class Seller < ApplicationRecord
     has_many :stocks
     has_many :sneakers
     
-    attr_accessor :vendor
-    
     validates :vendor, presence: true, uniqueness: true
     validates :logo_url, presence: true
 end
