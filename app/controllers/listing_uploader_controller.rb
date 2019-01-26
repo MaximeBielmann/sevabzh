0e38c71f1d7b3f78aa125a893545e6b0d6d055a4
EC2 Default User <ec2-user@ip-172-31-35-84.us-east-2.compute.internal>
2019-01-26 13:11:50 +0000

    listing uploader without sneaker create

--- app/controllers/listing_uploader_controller.rb
+++ app/controllers/listing_uploader_controller.rb
@@ -5,13 +5,13 @@ class ListingUploaderController < ApplicationController
     
     def upload
         CSV.foreach('public/upload.csv', headers: true) do |row|
-            Sneaker.where(sneakers_ref: row['sneakers_ref'], 
-                        brand: row['brand'], 
-                        title: row['title'], 
-                        color: row['color'], 
-                        img_url: row['img_url'],
-                        img_url2: row['img_url2'],
-                        img_url3: row['img_url3']).first_or_create
+            # Sneaker.where(sneakers_ref: row['sneakers_ref'], 
+            #             brand: row['brand'], 
+            #            title: row['title'], 
+            #            color: row['color'], 
+            #            img_url: row['img_url'],
+            #            img_url2: row['img_url2'],
+            #            img_url3: row['img_url3']).first_or_create
                            
             # Seller.where(vendor: row['seller'],
             #            logo_url: row['logo_url'], 
