aad6d50fd7452af9746bf6aa44e0944f76d66703
EC2 Default User <ec2-user@ip-172-31-35-84.us-east-2.compute.internal>
2019-02-03 19:50:26 +0000

    fix the search price

--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -27,6 +27,6 @@ class Sneaker < ApplicationRecord
  scope :search_brand, -> (search_brand) { where("lower(brand) LIKE ?", "%#{search_brand.to_s.downcase}%") }
  scope :search_title, -> (search_title) { where("lower(title) LIKE ?", "%#{search_title.to_s.downcase}%") }
  scope :search_color, -> (search_color) { where("lower(color) LIKE ?", "%#{search_color.to_s.downcase}%") }
- scope :search_price, -> (search_price) { joins(:stocks).where("price < ?", "#{search_price.to_s}").select(:id, :sneakers_ref, :brand, :title, :color, :img_url).distinct }
+ scope :search_price, -> (search_price) { joins(:stocks).where("price < ?", "#{search_price.to_f}").select(:id, :sneakers_ref, :brand, :title, :color, :img_url).distinct }
  scope :search_all, -> (search_all) { where("lower(sneakers_ref) AND lower(brand) AND lower(title) AND lower(color) LIKE ?", "%#{search_all.to_s.downcase}%") }
 end
