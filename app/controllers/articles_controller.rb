7e2c3539b03db0719e540f6454e88dcc4ad4e4e7
EC2 Default User <ec2-user@ip-172-31-35-84.us-east-2.compute.internal>
2019-02-04 16:47:38 +0000

    fix update articles#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
     @articles.article_title = params[:article_title]
     @articles.article_text = params[:article_text]
     @articles.article_datetime = params[:article_datetime]
-    @artiles.article_img = params[:article_img]
+    @articles.article_img = params[:article_img]
     @articles.save
     redirect_to "/blog/#{params[:id]}"
   end
