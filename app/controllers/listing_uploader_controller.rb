2227582e26f68000242c0faca80c89a34017c456
EC2 Default User <ec2-user@ip-172-31-35-84.us-east-2.compute.internal>
2019-03-30 23:17:45 +0000

    try to add .first to where

--- app/controllers/listing_uploader_controller.rb
+++ app/controllers/listing_uploader_controller.rb
@@ -88,7 +88,7 @@ class ListingUploaderController < ApplicationController
     def update_brands
         
         CSV.foreach('public/sneakers.csv', headers: true) do |row|
-            sneaky = Sneaker.where(sneakers_ref: row['sneakers_ref'])
+            sneaky = Sneaker.where(sneakers_ref: row['sneakers_ref']).first
             sneaky.update(brand_id: Brand.where(brand_title: row['brand']).ids)
             
             # Sneaker.where(sneakers_ref: row['sneakers_ref']).update_all(brand_id: Brand.where(brand_title: row['brand']).ids)
